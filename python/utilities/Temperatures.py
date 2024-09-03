8b563fee07abff64c79d545d11e6bf88f4708257
uvabtl <uvabtl@gmail.com>
2024-09-03 16:48:07 -0400

    Set up PID with UVA chiller and using module temps in place of box temp

--- python/utilities/Temperatures.py
+++ python/utilities/Temperatures.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from subprocess import Popen, PIPE
 import subprocess
 
-BAC = "Milano"
+BAC = "UVA"
 
 
 def read_box_temp():
@@ -22,6 +22,16 @@ def read_box_temp():
             raise ValueError("Could not read box temperature")
         else:
             return [float(result[2]),float(result[4]),float(result[6]),float(result[8]),float(result[10])]
+
+    if BAC == "UVA":
+        out = Popen(['tail','-n','1','/home/qaqcbtl/qaqc_jig/module_temps.log'],stdout=subprocess.PIPE)
+        result = (out.stdout.read().decode().rstrip()).split()
+        data_time = datetime.strptime(result[0]+" "+result[1].split('.')[0],"%Y-%m-%d %H:%M:%S")
+
+        if abs(data_time-now).total_seconds() > 600.:
+            raise ValueError("Could not read box temperature")
+        else:
+            return [float(r) for r in result[2:]]
 
 def read_box_temp_hum():
     now = datetime.now()
